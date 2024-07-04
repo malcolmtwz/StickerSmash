b67bfd49eade95a0eadcd495bc9ebe2a84d2de2b
Malcolm Tan <91950295+malcolmtwz@users.noreply.github.com>
2024-07-04 06:09:55 +0000

    My first commit

---
+++ StickerSmash/App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const bgImage = require('./assets/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imageSource={bgImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme = 'primary' label="Choose a photo"/>
+        <Button label="Use this photo"/>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer:{
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer:{
+    flex: 1/3,
+    alignItems: 'center',
+  }
+
+});
---
+++ StickerSmash/components/Button.js
@@ -0,0 +1,57 @@
+import { StyleSheet, View, Pressable, Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function Button ({ label, theme }){
+    if (theme === "primary"){
+        return(
+            <View style= {styles.buttonContainer}>
+                <Pressable 
+                    style={[styles.button, {backgroundColor: "#fff"}]} //inline styling takes priority over stylesheet
+                    onPress={() => alert('You pressed a button.')}
+                >
+                    <FontAwesome
+                        name="picture-o"
+                        size={18}
+                        color="#25292e"
+                        style={styles.buttonIcon}
+                    />
+                    <Text style={[styles.buttonLabel, { color: "#25292e"}]}>{label}</Text>
+                </Pressable>
+            </View>
+        );
+    }
+    return (
+        <View style={styles.buttonContainer}>
+            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+              <Text style={styles.buttonLabel}>{label}</Text>
+            </Pressable>
+          </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        width: 320,
+        height: 68,
+        marginHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 3,
+    },
+    button: {
+        borderRadius: 10,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection:'row',
+    },
+    buttonLabel: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    buttonIcon: {
+        padding: 8,
+    }
+})
